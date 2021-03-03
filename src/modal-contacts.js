f3eded5272768b1228f4807814060645a59f925b
ABoklogov <74319867+ABoklogov@users.noreply.github.com>
2021-03-03 22:16:06 +0300

    Добавил модалку Contacts

--- src/modal-contacts.js
+++ src/modal-contacts.js
@@ -1,15 +1,27 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    openModalBtn: document.querySelector('[data-contacts-modal-open]'),
+    closeModalBtn: document.querySelector('[data-contacts-modal-close]'),
+    modal: document.querySelector('[data-contacts-modal]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    document.body.classList.toggle('modal-open');
-    refs.modal.classList.toggle('is-opened');
+    document.body.classList.toggle('contacts-modal-open');
+    refs.modal.classList.toggle('is-contacts-hidden');
   }
 })();
+
+// -----для не отправки модалки 
+    
+// (() => {
+//         document.querySelector('.js-speaker-form').addEventListener('submit', e => {
+//         e.preventDefault();
+        
+//         new FormData(e.currentTarget).forEach((value, name) =>
+//         console.log(`${name}: ${value}`),
+//         );
+//         });
+// })();
